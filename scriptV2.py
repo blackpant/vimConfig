4fdb6f837883e23d3dd927c72194651e50d16bd5
Blackpant <saadi_4@hotmail.fr>
2016-04-21 16:03:52 +0200

    Ajout de quelques lignes de codes.

--- scriptV2.py
+++ scriptV2.py
@@ -57,27 +57,33 @@ print "--------------------------------------------------"
 print "Installation de  OhMyZsh."
 print "--------------------------------------------------"
 
-cmd = "sh"
-cmd_arg = "-c"
 cmd_arg2 = "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\""
 
 args = shlex.split(cmd_arg2)
 print "Commande : \n%r" % args
 codeReturn = subprocess.os.system(cmd_arg2)
-#re = subprocess.Popen(args)
-
 
 subprocess.os.system("cd ~/.oh-my-zsh/custom && mkdir themes")
 
-
 ha = "git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k"
-
-targs = shlex.split(ha)
 
 toto = subprocess.os.system(ha)
 
-#installation pwerline-fonts
+#installation powerline-fonts
+#installation via https://github.com/powerline/fonts et ensuite lancer ./install.sh
+
+subprocess.call(["cd", "~/Documents/"])
+subprocess.call(["git", "clone", "https://github.com/powerline/fonts"])
+subprocess.call(["cd","fonts", "&&", "./install.sh"])
+
+
 #installation guake-tomorrow-night
 #changement de theme guake,
     # ./set_tomorrow_night.sh
+
+subprocess.call(["git", "clone", "https://github.com/carwin/guake-tomorrow-night"])
+subprocess.call(["cd", "quake-tomorrow-night"])
+subprocess.call((["./set_tomorrow_night.sh"])
+
 #changement de la police de Guake par une de Powerline.
+#Meslo* est pas mal.
