98cb53d05c742a4cdc68194d2936b5581e926f79
Blackpant <saadi_4@hotmail.fr>
2016-04-21 11:56:00 +0200

    Add install Exxuberantctags + pip

--- scriptV2.py
+++ scriptV2.py
@@ -23,6 +23,17 @@ except Exception:
 	print "Une erreur est survenue.\nreturncode : %r" % returncode
 
 returncode = 0
+
+print "--------------------------------------------------"
+print "--------------------------------------------------"
+print "Installation de Python-pip + Exuberant-ctags."
+print "--------------------------------------------------"
+
+subprocess.call(["sudo", "apt-get", "install", "python-pip"])
+subprocess.call(["sudo", "apt-get", "install", "Exuberant-ctags"])
+
+
+
 print "--------------------------------------------------"
 print "--------------------------------------------------"
 print "Installation de Vim Spf13 + copie fichiers de configs."
@@ -31,8 +42,11 @@ print "--------------------------------------------------"
 try:
 	#returncode = subprocess.os.system("curl http://j.mp/spf13-vim3 -L -o - | sh")
 
-	subprocess.os.system("cp .vimrc $HOME")
-	subprocess.os.system("cp .vimrc.bundles $HOME")
+    subprocess.call(["sudo", "apt-get", "install", "vim-nox"])
+    subprocess.os.system("cp .vimrc $HOME")
+    subprocess.os.system("cp .vimrc.bundles $HOME")
+    subprocess.call(["vim", "+PluginInstall", "+qall"])
+
 except Exception:
 	print "Une erreur est survenue!!"
 	print "(Spf13) returncode : %r" % returncode
